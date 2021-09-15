ce8ef12bf3fb52dba7c4904785696399c3451a4e
Joe <jmummdbd@att.net>
2021-09-14 20:51:23 -0700

    Finished Module 6 Images to Docker Compose
    One remaining bug, calling the weather page from the UI throws a NullReferenceException in the Core PollyHelper.AddResilencePolices onRetry.

--- Globomantics.IdentityServer/Initialization/InitialConfiguration.cs
+++ Globomantics.IdentityServer/Initialization/InitialConfiguration.cs
@@ -9,66 +9,67 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Globomantics.IdentityServer.Initialization
 {
-    // based on code found here: 
-    // https://github.com/IdentityServer/IdentityServer4.Demo/blob/main/src/IdentityServer4Demo/Config.cs
-    public static class InitialConfiguration
+  // based on code found here: 
+  // https://github.com/IdentityServer/IdentityServer4.Demo/blob/main/src/IdentityServer4Demo/Config.cs
+  public static class InitialConfiguration
+  {
+    public static void PopulateDatabaseIfEmpty(this IApplicationBuilder app)
     {
-        public static void PopulateDatabaseIfEmpty(this IApplicationBuilder app)
+      using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+      {
+        var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+        if (!context.Clients.Any())
         {
-            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-            {
-                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-
-                if (!context.Clients.Any())
-                {
-                    foreach (var client in GetClients())
-                    {
-                        context.Clients.Add(client.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
-
-                if (!context.IdentityResources.Any())
-                {
-                    foreach (var resource in GetIdentityResources())
-                    {
-                        context.IdentityResources.Add(resource.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
-
-                if (!context.ApiResources.Any())
-                {
-                    foreach (var resource in GetApis())
-                    {
-                        context.ApiResources.Add(resource.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
-
-                if (!context.ApiScopes.Any())
-                {
-                    foreach (var scope in GetApiScopes())
-                    {
-                        context.ApiScopes.Add(scope.ToEntity());
-                    }
-
-                    context.SaveChanges();
-                }
-            }
+          foreach (var client in GetClients())
+          {
+            context.Clients.Add(client.ToEntity());
+          }
+          context.SaveChanges();
         }
 
-        public static IEnumerable<Client> GetClients()
+        if (!context.IdentityResources.Any())
         {
-            
-            return new List<Client>
+          foreach (var resource in GetIdentityResources())
+          {
+            context.IdentityResources.Add(resource.ToEntity());
+          }
+          context.SaveChanges();
+        }
+
+        if (!context.ApiResources.Any())
+        {
+          foreach (var resource in GetApis())
+          {
+            context.ApiResources.Add(resource.ToEntity());
+          }
+          context.SaveChanges();
+        }
+
+        if (!context.ApiScopes.Any())
+        {
+          foreach (var scope in GetApiScopes())
+          {
+            context.ApiScopes.Add(scope.ToEntity());
+          }
+
+          context.SaveChanges();
+        }
+      }
+    }
+
+    public static IEnumerable<Client> GetClients()
+    {
+
+      return new List<Client>
             {
                 new Client
                 {
                     ClientId = "globo-core",
                     ClientName = "Globomantics Core UI (Code with PKCE)",
 
-                    RedirectUris = { "https://localhost:44320/signin-oidc" },
+                    //RedirectUris = { "https://localhost:44320/signin-oidc" },
+                    RedirectUris = { "https://www-local.globomantics.com:44395/signin-oidc" },
                     PostLogoutRedirectUris = { "https://notused" },
 
                     ClientSecrets = { new Secret("secret".Sha256()) },
@@ -83,46 +84,48 @@ namespace Globomantics.IdentityServer.Initialization
                 {
                     ClientId = "globo-swagger",
                     ClientName = "Swagger UI for the Globomantics API",
-                    RedirectUris = { "https://localhost:44376/oauth2-redirect.html" },
+                    //RedirectUris = { "https://localhost:44376/oauth2-redirect.html" },
+                    RedirectUris = { "https://www-local.globomantics.com:44395/api/oauth2-redirect.html" },
                     PostLogoutRedirectUris = { "https://notused" },
 
                     RequireConsent = false,
-                    AllowedCorsOrigins = new List<string> { "https://localhost:44376" },
+                    //AllowedCorsOrigins = new List<string> { "https://localhost:44376" },
+                    AllowedCorsOrigins = new List<string> { "https://www-local.globomantics.com:44395" },
                     AllowedGrantTypes = GrantTypes.Code,
                     RequirePkce = true,
                     RequireClientSecret = false,
                     AllowedScopes = { "openid", "profile", "email", "glob_profile", "glob_api"   }
                 }
             };
-        }
-        public static IEnumerable<IdentityResource> GetIdentityResources()
-        {
-            return new List<IdentityResource>
+    }
+    public static IEnumerable<IdentityResource> GetIdentityResources()
+    {
+      return new List<IdentityResource>
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
                 new IdentityResources.Email(),
                 new IdentityResource("glob_profile", new List<string>{"MfaEnabled", "CompanyId", JwtClaimTypes.Role})
             };
-        }
+    }
 
-        public static IEnumerable<ApiScope> GetApiScopes()
-        {
-            return new List<ApiScope>
+    public static IEnumerable<ApiScope> GetApiScopes()
+    {
+      return new List<ApiScope>
             {
                 new ApiScope("glob_api"),
             };
-        }
+    }
 
-        public static IEnumerable<ApiResource> GetApis()
-        {
-            return new List<ApiResource>
+    public static IEnumerable<ApiResource> GetApis()
+    {
+      return new List<ApiResource>
             {
                 new ApiResource("glob_api", "Globomantics API")
                 {
                     Scopes = { "glob_api" }
                 }
             };
-        }
     }
+  }
 }
